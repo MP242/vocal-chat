60e3ff569dadd76a60495d6dd064cf167e7d348b
MP242 <marc.pollet@epitech.eu>
2024-03-01 13:50:46 +0400

    quick fix STT serverActions4

--- app/serverActions/speechToText.ts
+++ app/serverActions/speechToText.ts
@@ -2,22 +2,26 @@
 
 import OpenAI from "openai";
 import fs from "fs";
+import { Readable } from "stream";
 
 export const speechToText = async (base64Audio: string): Promise<string> => {
   try {
+    console.log("server --> base64Audio", base64Audio)
     const openai = new OpenAI();
 
     // Convert the base64 audio data to a Buffer
-    const audio = Buffer.from(base64Audio, "base64");
+    const audioBuffer = Buffer.from(base64Audio, "base64");
 
     // Define the file path for storing the temporary WAV file
     const filePath = "public/audio/input.wav";
 
     // Write the audio data to a temporary WAV file synchronously
-    fs.writeFileSync(filePath, audio);
+    fs.writeFileSync(filePath, audioBuffer);
 
     // Create a readable stream from the temporary WAV file
     const readStream = fs.createReadStream(filePath);
+    console.log("readStream", readStream)
+    console.log("typeof readStream", typeof(readStream))
 
     const data = await openai.audio.transcriptions.create({
       file: readStream,
