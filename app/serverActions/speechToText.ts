547de9f553d5d6a3a3475f14ba7770600eec53f0
MP242 <marc.pollet@epitech.eu>
2024-03-01 16:38:20 +0400

    remove storing file by passing buffer to openai

--- app/serverActions/speechToText.ts
+++ app/serverActions/speechToText.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import OpenAI from "openai";
+import OpenAI, {toFile} from "openai";
 import fs from "fs";
 import path from "path";
 
@@ -16,27 +16,12 @@ export const speechToText = async (base64Audio: string): Promise<string> => {
     console.log("server --> base64Audio", base64Audio)
     const openai = new OpenAI();
 
-    // Convert the base64 audio data to a Buffer
     const audioBuffer = Buffer.from(base64Audio, "base64");
 
-    // Define the file path for storing the temporary WAV file
-    const filePath = "public/audio/input.wav";
-
-    // Write the audio data to a temporary WAV file synchronously
-    fs.writeFileSync(filePath, audioBuffer);
-
-    // Create a readable stream from the temporary WAV file
-    const readStream = fs.createReadStream(filePath);
-    console.log("readStream", readStream)
-    console.log("typeof readStream", typeof(readStream))
-
     const data = await openai.audio.transcriptions.create({
-      file: readStream,
+      file: await toFile(audioBuffer, "audio.wav"),
       model: "whisper-1",
     });
-    console.log("data.text", data.text);
-    // Remove the temporary file after successful processing
-    fs.unlinkSync(filePath);
 
     return data.text;
   } catch (error: any) {
