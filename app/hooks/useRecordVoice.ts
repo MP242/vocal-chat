ad944cb9e4300d5caea3175b70082672cf4f1245
MP242 <marc.pollet@epitech.eu>
2024-03-01 13:26:00 +0400

    quick fix STT serverActions2

--- app/hooks/useRecordVoice.ts
+++ app/hooks/useRecordVoice.ts
@@ -30,8 +30,8 @@ export const useRecordVoice = () => {
 
   const getText = async (base64data:string) => {
     try {
-      console.log("base64data",base64data)
-      const text:string= await speechToText(base64data);
+      const text= await speechToText(base64data);
+
       console.log("from useRecordVoice text",text)
       setText(text);
     } catch (error) {
